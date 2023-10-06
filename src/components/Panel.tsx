86e1b8223e7c4e020dcc525e3701192ce79ed231
Raudel Ravelo <raudelravelo91@gmail.com>
2023-10-06 15:21:48 -0400

    fix disable on add and remove buttons

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useState, useRef } from 'react';
 import Message from './Message';
 
 function Panel() {
@@ -6,16 +6,20 @@ function Panel() {
 
     const [inputValue, setInputValue] = useState('');
 
+    const inputRef = useRef<HTMLInputElement>(null);
+
     function handleClick() {
         const newElement = <div key={elements.length}><Message message={inputValue} /></div>;
         const newElements = [...elements, newElement];
         setElements(newElements);
         setInputValue('');
+        inputRef.current?.focus();
     }
 
     function handleRemove() {
         const newElements = elements.slice(0, -1);
         setElements(newElements);
+        inputRef.current?.focus();
     }
 
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -25,13 +29,13 @@ function Panel() {
     return (
         <div style={{ margin: 'auto', marginTop: '20px', minHeight: '100vh', overflow: 'auto' }}>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <input style={{ margin: '5px' }} type="text" value={inputValue} onChange={handleInputChange} />
-                <button style={{ margin: '5px' }} onClick={handleClick}>Add Value</button>
-                <button style={{ margin: '5px' }} onClick={handleRemove}>Remove Value</button>
+                <input style={{ margin: '5px' }} type="text" value={inputValue} onChange={handleInputChange} ref={inputRef} />
+                <button style={{ margin: '5px' }} onClick={handleClick} disabled={inputValue.length === 0}>Add Value</button>
+                <button style={{ margin: '5px' }} onClick={handleRemove} disabled={elements.length === 0}>Remove Value</button>
             </div>
             <div>
-                {elements.map((value, index) => (
-                    <div key={index}>{value}</div>
+                {elements.map((message, index) => (
+                    <div key={index}>{message}</div>
                 ))}
             </div>
         </div>
