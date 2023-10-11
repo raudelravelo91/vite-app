ecbc38cb06f4a26791bfce71cfb63c5980912606
Raudel Ravelo <raudelravelo91@gmail.com>
2023-10-11 16:39:09 -0400

    use useForm custom hook

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useState, useRef, ButtonHTMLAttributes } from 'react';
 import Message, { MessageProps } from './Message';
+import {useForm} from "react-hook-form";
 
 interface PanelProps{
     OnRemove: () => void;
@@ -8,41 +9,33 @@ interface PanelProps{
 
 function Panel({OnRemove, OnAdd}: PanelProps): JSX.Element {
     const [elements, setElements] = useState<MessageProps[]>([]);
-    const inputRef = useRef<HTMLInputElement>(null);
-    const addRef = useRef<HTMLButtonElement>(null);
-    updateDisableAdd();
+    const {register, handleSubmit, setFocus, setValue} = useForm();
 
-    function handleAdd() {
-        const newElement = { message: inputRef.current?.value??"" };
+    function handleAdd(data: any) {
+        const newElement = { message: data.name };
         const newElements = [...elements, newElement];
         setElements(newElements);
+        setValue('name', '');
+        setFocus('name');
         OnAdd();
-        inputRef.current?.focus();
-        inputRef.current!.value = "";
     }
 
     function handleRemove() {
         const newElements = elements.slice(0, -1);
         setElements(newElements);
+        setFocus('name');
         OnRemove();
-        inputRef.current?.focus();
-    }
-
-    function updateDisableAdd() { 
-        addRef.current!.disabled = inputRef.current?.value === "";
-    }
-
-    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-        updateDisableAdd();
     }
 
     return (
         <div style={{ margin: 'auto', marginTop: '20px', minHeight: '100vh', overflow: 'auto' }}>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <input style={{ margin: '5px' }} type="text" onChange={handleInputChange} ref={inputRef} />
-                <button style={{ margin: '5px' }} onClick={handleAdd} ref={addRef}>Add Value</button>
-                <button style={{ margin: '5px' }} onClick={handleRemove} disabled={elements.length === 0}>Remove Value</button>
-            </div>
+             <form onSubmit={handleSubmit(handleAdd)}>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <input type="text" style={{ margin: '5px' }} {...register('name', { required: true })}/>
+                    <button style={{ margin: '5px' }} type="submit">Add Value</button>
+                    <button style={{ margin: '5px' }} type="button" onClick={handleRemove} disabled={elements.length === 0}>Remove Value</button>
+                </div>
+            </form>
             <div>
                 {elements.map((prop, index) => 
                     <div key={"div" + index}>
